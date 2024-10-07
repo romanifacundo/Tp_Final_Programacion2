19669ea18f28b114c48fc1c9dcf3209045ba5cc4
romanifacundo <facundoromani90@gmail.com>
2024-10-07 19:52:05 -0300

    Jorge Sager estructura de clases localidad-provincia

--- TP_Final_Programación_2/TP_Final_Programación_2/Validacion.cs
+++ TP_Final_Programación_2/TP_Final_Programación_2/Validacion.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace TP_Final_Programación_2
 {
     internal class Validacion
-    {
+    { 
         /// <summary>
         /// Pensamos una estructura mas limpia de codigo y mejor organizada centralizando las Validaciones en esta clase.
         /// </summary>
